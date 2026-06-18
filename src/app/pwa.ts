f58c0fd29c63b1651606e7826eb1add7233efb09
natuselit <natuselit@users.noreply.github.com>
2026-06-18 20:43:59 +0300

    Force reload after PWA update

--- src/app/pwa.ts
+++ src/app/pwa.ts
@@ -1,6 +1,17 @@
 import { registerSW } from 'virtual:pwa-register';
 
 export function registerAppServiceWorker() {
+  let refreshing = false;
+
+  navigator.serviceWorker?.addEventListener('controllerchange', () => {
+    if (refreshing) {
+      return;
+    }
+
+    refreshing = true;
+    window.location.reload();
+  });
+
   const updateServiceWorker = registerSW({
     immediate: true,
     onNeedRefresh() {
